b1928183f4784680911c0d91aa08e09f8b80f6b8
David Lu <david-lu@hotmail.com>
2025-05-04 22:58:58 -0700

    sure

---
+++ setup.sh
@@ -0,0 +1,10 @@
+conda create -n skylark python=3.11
+conda activate skylark
+conda install pytorch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 pytorch-cuda=12.4 -c pytorch -c nvidia
+
+pip install -r requirements.txt
+
+pushd model\segment_anything_2
+python setup.py build_ext --inplace
+popd
+
