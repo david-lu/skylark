f5810eddd8ed72ff4e2937e77f8c316632d5caf2
David Lu <david-lu@hotmail.com>
2025-05-18 08:12:37 -0700

    finally workin'

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
-pyenv global 3.11
+pyenv global 3.10.13
 python -m venv .venv
 source .venv/bin/activate
 
 sudo apt install -y libffi-dev build-essential zlib1g-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev libncurses-dev libncursesw5-dev xz-utils tk-dev
-pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cu124
+pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cu121
+
 
 pip install -r requirements.txt
 
